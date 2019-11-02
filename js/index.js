11828d84dbc246cd6ee343d34e0cd9b92eee2fda
b1zz3 <donhager23@gmail.com>
2019-11-02 13:45:10 -0400

    Started adding eventListeners, worked out some bugs

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const mseWhl = document.getElementsByClassName("center");
+window.addEventListener("wheel", () => {
+    console.log("It works");
+    // mseWhl.style.cursor = "not-allowed"; not working
+});
+
+const pics = document.querySelectorAll("img");
+pics.forEach(pic => {
+    pic.addEventListener('mouseenter', () => {
+        pic.style.transform="scale(1.2)";
+        pic.style.transition="all 0.3s";
+    })
+})
+
+pics.forEach(pic => {
+    pic.addEventListener('mouseleave', () => {
+        pic.style.transform = "scale(1)";
+        pic.style.transition = "all 0.3s";
+    })
+})
+    // links.style.transform = "rotateY(180deg)";
