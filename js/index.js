087aa44766eb3f993434840f90d3df9322f63e41
b1zz3 <donhager23@gmail.com>
2019-11-02 14:00:21 -0400

    Added mouseover event

--- js/index.js
+++ js/index.js
@@ -8,15 +8,24 @@ window.addEventListener("wheel", () => {
 const pics = document.querySelectorAll("img");
 pics.forEach(pic => {
     pic.addEventListener('mouseenter', () => {
-        pic.style.transform="scale(1.2)";
-        pic.style.transition="all 0.3s";
+        // pic.style.transform = "scale(1.2)";
+        pic.style.transform = "rotateY(180deg)";
+        pic.style.transition = "all 0.3s";
     })
 })
 
 pics.forEach(pic => {
     pic.addEventListener('mouseleave', () => {
-        pic.style.transform = "scale(1)";
+        // pic.style.transform = "scale(1)";
         pic.style.transition = "all 0.3s";
+        pic.style.transform = "rotateY(360deg)";
     })
 })
-    // links.style.transform = "rotateY(180deg)";
+
+const links = document.querySelectorAll("a");
+links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        link.style.transition = "all 0.3s";
+        link.style.transform = "rotateX(180deg)";
+    })
+})
